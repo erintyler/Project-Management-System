8a8736154de3b928945efcb788f32f284aa0358b
Saxolotl <erin@saxolotl.xyz>
2020-05-22 13:30:12 +0100

    Updated Theming

--- src/index.jsx
+++ src/index.jsx
@@ -9,12 +9,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { ThemeProvider, CssBaseline, createMuiTheme, responsiveFontSizes } from '@material-ui/core';
-import { deepPurple, purple, blue, deepOrange, grey, blueGrey, yellow, amber, orange } from '@material-ui/core/colors';
+import { deepPurple, purple, blue, deepOrange, grey, blueGrey, yellow, amber, orange, indigo, green } from '@material-ui/core/colors';
 
 let theme = createMuiTheme({
   palette: {
     type: "dark",
-    primary: orange
+    primary: {
+      main: '#47c7f0',
+    }
   }
 });
 
